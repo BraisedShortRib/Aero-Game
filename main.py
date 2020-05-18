2d8545a70ee782fd7d96c3e851eb02c6b799236e
kakanishi <kenneth.t.miura@gmail.com>
2020-05-18 12:49:57 -0400

    added missile spawning and start/end screen

--- main.py
+++ main.py
@@ -1,40 +1,73 @@
 import pygame
 import sys
+import random
 from plane import Plane
 from projectile import Projectile
 
 
-#TODO:
-# randomize missile starting position
-# Create missiles on a time interval
-# Maybe make the missiles explode if they hit you? Show that you fucked up
-# Make sure this still compiles on windows
-# Make it so it will actually exit the game if you click the x
+def shoot_projectile(projectiles_group, screen_width, screen_height):
+    y = random.randint(0, screen_height)
+    max_speed = 8
+    speed = random.randint(1, max_speed)
+    projectile = Projectile(screen_width, y, speed)
+    projectiles_group.add(projectile)
+
+
 def main():
+    random.seed()
     pygame.init()
     monitor_info = pygame.display.Info()
     size = width, height = monitor_info.current_w, monitor_info.current_h
 
     screen = pygame.display.set_mode(size)
+
+    start_background = pygame.image.load(
+        "resources/start_background.jpg").convert()
+    start_background = pygame.transform.scale(start_background,
+                                              (width, height))
     background = pygame.image.load("resources/background.jpg").convert()
     background = pygame.transform.scale(background, (width, height))
-    screen.blit(background, (0, 0))
+
+    end_background = pygame.image.load(
+        "resources/end_background.jpg").convert()
+    end_background = pygame.transform.scale(end_background, (width, height))
     plane = Plane()
-    projectile = Projectile(width)
     projectiles = pygame.sprite.Group()
-    projectiles.add(projectile)
+    shoot_projectile(projectiles, width, height)
     plane_group = pygame.sprite.Group()
     plane_group.add(plane)
 
     # Change behavior so key registers as pressed again if its held, every 1 ms
     pygame.key.set_repeat(1)
+    PROJECTILE_EVENT = pygame.USEREVENT + 1
+    time_step = 300
+    pygame.time.set_timer(PROJECTILE_EVENT, time_step)
 
-    while True:
+
+    start_screen = True
+    running_game = True
+    end_screen = False
+
+    screen.blit(start_background, (0,0))
+    pygame.display.flip()
+    while start_screen:
         pygame.event.pump()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print("exiting")
                 sys.exit()
+        keypressed = pygame.key.get_pressed()
+        if keypressed[pygame.K_SPACE]:
+            start_screen = False
+
+    screen.blit(background, (0, 0)) 
+    pygame.display.flip()
+    while running_game:
+        pygame.event.pump()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == PROJECTILE_EVENT:
+                shoot_projectile(projectiles, width, height)
         keypressed = pygame.key.get_pressed()
         if keypressed[pygame.K_DOWN] and not plane.out_bottom_bounds(height):
             plane.fly_down()
@@ -45,7 +78,9 @@ def main():
         if keypressed[pygame.K_LEFT] and not plane.out_left_bounds():
             plane.fly_left()
         if pygame.sprite.spritecollide(plane, projectiles, False):
-            print("colliding")
+            running_game = False
+            end_screen = True
+            print("switching to end screen")
         else:
             print("not colliding")
         plane_group.update()
@@ -54,6 +89,13 @@ def main():
         plane_group.draw(screen)
         projectiles.draw(screen)
         pygame.display.flip()
+
+    screen.blit(end_background, (0, 0))
+    pygame.display.flip()
+    while end_screen:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == "__main__":
--- projectile.py
+++ projectile.py
@@ -2,13 +2,13 @@ import pygame
 
 
 class Projectile(pygame.sprite.Sprite):
-    def __init__(self, width):
+    def __init__(self, x_start_pos, y_start_pos, speed=3):
         super(Projectile, self).__init__()
         self.image = pygame.image.load("resources/projectile.png")
         self.rect = self.image.get_rect()
         # positions below will be randomized eventually
-        self.rect.x = width
-        self.rect.y = 50
-        self.speed = 3
+        self.rect.x = x_start_pos
+        self.rect.y = y_start_pos
+        self.speed = speed
     def update(self):
         self.rect.x -=3
